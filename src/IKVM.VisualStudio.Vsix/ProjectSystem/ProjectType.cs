64bc79393314b793eb111c1fe80a8ae042fd2d3d
Jerome Haltom <jhaltom@alethic.solutions>
2025-03-29 16:57:03 -0500

    1

--- src/IKVM.VisualStudio.Vsix/ProjectSystem/ProjectType.cs
+++ src/IKVM.VisualStudio.Vsix/ProjectSystem/ProjectType.cs
@@ -10,12 +10,12 @@ namespace IKVM.VisualStudio.Vsix.ProjectSystem
     {
 
         /// <summary>
-        ///     A <see cref="string"/> representing the IKVM project type based on the Common Project System (CPS).
+        /// A <see cref="string"/> representing the IKVM project type based on the Common Project System (CPS).
         /// </summary>
         public const string Ikvm = "DAEA77DE-8320-43BA-BA7C-EF5C12478AB5";
 
         /// <summary>
-        ///     A <see cref="Guid"/> representing the IKVM project type based on the Common Project System (CPS).
+        /// A <see cref="Guid"/> representing the IKVM project type based on the Common Project System (CPS).
         /// </summary>
         public static readonly Guid IkvmGuid = new(Ikvm);
 
