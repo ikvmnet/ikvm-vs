df79c2980f8a0eb64d7543d75cbeb4048cd93ba6
Jerome Haltom <jhaltom@alethic.solutions>
2026-05-29 10:35:51 -0500

    d

---
+++ src/IKVM.VisualStudio.Vsix/Images/IkvmImageMonikers.cs
@@ -0,0 +1,17 @@
+using System;
+
+using Microsoft.VisualStudio.Imaging.Interop;
+
+namespace IKVM.VisualStudio.Vsix.Images
+{
+
+    internal static class IkvmImageMonikers
+    {
+
+        private static readonly Guid ManifestGuid = new Guid("{99a611b0-1654-4403-b5b6-7b72d999d621}");
+
+        public static ImageMoniker ProjectIcon => new ImageMoniker { Guid = ManifestGuid, Id = 0 };
+
+    }
+
+}
